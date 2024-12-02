f84c3daec4b6bcf9fc266740c9b9bfac4bea873a
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-12-02 15:15:47 +0800

    Update add-author.php

--- Online Bookstore System in PHP/add-author.php
+++ Online Bookstore System in PHP/add-author.php
@@ -11,6 +11,7 @@ if (isset($_SESSION['user_id']) &&
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/png" href="img/dplogo.png">
 	<title>Add Author</title>
 
     <!-- bootstrap 5 CDN-->
@@ -18,44 +19,45 @@ if (isset($_SESSION['user_id']) &&
 
     <!-- bootstrap 5 Js bundle CDN-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-
+	<!--<link rel="stylesheet" href="css/custom.css"> -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+	<link rel="stylesheet" href="css/mycustomadmin.css">
+  <link rel="stylesheet" href="css/mycustomadd.css">
 </head>
 <body>
-	<div class="container">
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		  <div class="container-fluid">
-		    <a class="navbar-brand" href="admin.php">Admin</a>
-		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		      <span class="navbar-toggler-icon"></span>
-		    </button>
-		    <div class="collapse navbar-collapse" 
-		         id="navbarSupportedContent">
-		      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-		        <li class="nav-item">
-		          <a class="nav-link" 
-		             aria-current="page" 
-		             href="index.php">Store</a>
-		        </li>
-		        <li class="nav-item">
-		          <a class="nav-link" 
-		             href="add-book.php">Add Book</a>
-		        </li>
-		        <li class="nav-item">
-		          <a class="nav-link" 
-		             href="add-category.php">Add Category</a>
-		        </li>
-		        <li class="nav-item">
-		          <a class="nav-link active" 
-		             href="add-author.php">Add Author</a>
-		        </li>
-		        <li class="nav-item">
-		          <a class="nav-link" 
-		             href="logout.php">Logout</a>
-		        </li>
-		      </ul>
-		    </div>
-		  </div>
-		</nav>
+<!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark shadow fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">
+        <img src="img/logo.png" alt="Logo" class="rounded-circle me-2" width="50"> 
+        <span class="fw-bold text-light">Admin</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+         <span class="navbar-toggler-icon" style="background-color: #3D5300; background-image: url('data:image/svg+xml;charset=UTF8,%3Csvg xmlns%3D%27http%3A//www.w3.org/2000/svg%27 viewBox%3D%270 0 30 30%27%3E%3Cpath stroke%3D%27white%27 stroke-width%3D%272%27 d%3D%27M4 7h22M4 15h22M4 23h22%27/%3E%3C/svg%3E');"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a href="index.php" class="nav-link text-light" aria-current="page">Store</a>
+          </li>
+          <li class="nav-item">
+            <a href="add-book.php" class="nav-link text-light">Add Book</a>
+          </li>
+          <li class="nav-item">
+            <a href="add-category.php" class="nav-link text-light">Add Category</a>
+          </li>
+          <li class="nav-item">
+            <a href="add-author.php" class="nav-link active">Add Author</a>
+          </li>
+          <li class="nav-item">
+            <a href="logout.php" class="nav-link text-light">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+	<div class="container box">
      <form action="php/add-author.php"
            method="post" 
            class="shadow p-4 rounded mt-5"
